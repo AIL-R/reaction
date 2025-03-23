f3b080a4f2e3a95371943c7167c96f7d73fe4c66
AIL <15840735287@163.com>
2025-03-23 16:48:30 +0800

    Add build files after fixing index.js and index.html

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // Optional, if you have custom styles
-import App from './App'; // Ensure that this component exists
-import reportWebVitals from './reportWebVitals'; // Optional, if using web vitals
+import './index.css'; // 导入样式文件
+import App from './App'; // 导入主应用组件
 
+// 确保 React 应用渲染到页面的根元素
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
